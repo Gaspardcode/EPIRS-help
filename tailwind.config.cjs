6f1ab5c869de9a5bcd3e1f8c68b23d02ddcd2f9e
gabriellegris <gabriel.dezon@epita.fr>
2024-12-26 19:42:43 +0100

    please

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
-module.exports = {
+// tailwind.config.js
+export default {
   content: [
     "./index.html",
     "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust this line to include any file types that might contain Tailwind classes
